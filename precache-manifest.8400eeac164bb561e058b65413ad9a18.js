a2ed18a8f16e4d3914c5f721028373c7a1c6cef8
galkremer1 <galkremer1@walla.co.il>
2019-03-18 23:26:24 +0200

    Updates

---
+++ precache-manifest.8400eeac164bb561e058b65413ad9a18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b2427a60a096fe75034",
+    "url": "/currencyEchange/static/js/runtime~main.48e9a85f.js"
+  },
+  {
+    "revision": "39d8ba4b83b7a5838665",
+    "url": "/currencyEchange/static/js/main.092032ed.chunk.js"
+  },
+  {
+    "revision": "d36bbd73519e55ae1a69",
+    "url": "/currencyEchange/static/js/2.c979a2e3.chunk.js"
+  },
+  {
+    "revision": "39d8ba4b83b7a5838665",
+    "url": "/currencyEchange/static/css/main.5224231d.chunk.css"
+  },
+  {
+    "revision": "0deec6214be9ab604b38a66eb5b08f9f",
+    "url": "/currencyEchange/index.html"
+  }
+];
